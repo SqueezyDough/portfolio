dd85948adc13a4a05d28f32924980d5d77c06929
leroy van Biljouw <leroy@superherocheesecake.com>
2022-02-13 12:39:14 +0100

    feat: add hero animation

--- src/components/UI/organisms/Hero/index.js
+++ src/components/UI/organisms/Hero/index.js
@@ -7,30 +7,39 @@ import Spheres from '@/UI/molecules/Spheres'
 import styles from './Hero.module.scss'
 import ScrollIndicator from '@/UI/molecules/ScrollIndicator'
 
-// import { easeOutSine } from '@/ease'
-
 const Hero = ({ title, images }) => {
   const words = _.split(title, ' ')
 
   return (
-    <section className={styles.hero}>
+    <section className={styles.container}>
+      <motion.div
+        className={styles.canvas}
+        animate={{
+          y: [400, 0, 0, 0],
+          clipPath: ['circle(0%)', 'circle(20%)', 'circle(100%)'],
+        }}
+        transition={{ duration: 5, delay: 1, timings: [0, 0.2, 0.9, 1] }}
+      >
+        <ImageDistorted image={images[0]} />
+      </motion.div>
+
       <motion.div
         className={styles.spheresContainer}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ delay: 1 }}
+        transition={{ delay: 5 }}
       >
         <Spheres className={styles.spheres} texture={images[1]} />
       </motion.div>
 
       <ParallaxContainer inputRange={500} outputRange={-100}>
-        <motion.h1 className={styles.title}>
+        <motion.h1 className={styles.title} animate={{ y: [50, 0] }}>
           {words.map((word, i) => (
             <div key={i} className={styles.wordWrapper}>
               <motion.span
                 className={styles.word}
-                initial={{ yPercent: 100 }}
-                animate={{ yPercent: 0 }}
+                animate={{ y: [200, 0] }}
+                transition={{ delay: i * 0.5 }}
               >
                 {word}
               </motion.span>
@@ -38,10 +47,6 @@ const Hero = ({ title, images }) => {
           ))}
         </motion.h1>
       </ParallaxContainer>
-
-      <div className={styles.canvas}>
-        <ImageDistorted image={images[0]} />
-      </div>
 
       <footer className={styles.footer}>
         <ScrollIndicator />
